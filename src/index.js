272d5d02d311d24dc8b8a5f98622c692c7dd1442
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2018-03-20 15:01:35 -0400

    init

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import 'babel-polyfill';
+import jwt from 'jsonwebtoken';
+import verifyJWT from './verifyJWT';
+
+export default function({ required, egoURL = process.env.EGO_API }) {
+  return async (req, res, next) => {
+    const { authorization } = req.headers;
+    const token = authorization ? authorization.split(' ')[1] : req.query.key;
+    const valid = await verifyJWT({ token, egoURL });
+
+    if (!valid && required) {
+      res.status(401).send('unauthorized');
+    } else {
+      req.jwt = { ...jwt.decode(token), valid };
+      next();
+    }
+  };
+}
---
+++ src/verifyJWT.js
@@ -0,0 +1,21 @@
+import memoize from 'memoizee';
+import axios from 'axios';
+import ms from 'ms';
+import jwt from 'jsonwebtoken';
+import urlJoin from 'url-join';
+
+const getKey = memoize(
+  async egoURL => {
+    const response = await axios.get(urlJoin(egoURL, 'oauth/token/public_key'));
+    return response.data;
+  },
+  {
+    maxAge: ms('3h'),
+    preFetch: true,
+  },
+);
+
+export default async function({ token, egoURL }) {
+  const key = await getKey(egoURL);
+  return jwt.verify(token, key);
+}
