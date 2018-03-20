43c386cecd2a707f02179e435dab5d4792dde3f0
Jeffrey Burt <jeffrey.burt@oicr.on.ca>
2018-03-20 16:00:50 -0400

    don't check if it doesn't exist

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ export default function({ required, egoURL = process.env.EGO_API }) {
   return async (req, res, next) => {
     const { authorization } = req.headers;
     const token = authorization ? authorization.split(' ')[1] : req.query.key;
-    const valid = await verifyJWT({ token, egoURL });
+    const valid = token && (await verifyJWT({ token, egoURL }));
 
     if (!valid && required) {
       res.status(401).send('unauthorized');
