9c745ffd6b8d82bd698d6eae922decb9b1890d70
Minh <hlminh2000@gmail.com>
2018-04-03 16:43:14 -0400

    checks request body for authorization data and adds doc

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
-import jwt from 'jsonwebtoken';
-import verifyJWT from './verifyJWT';
+import jwt from "jsonwebtoken";
+import verifyJWT from "./verifyJWT";
 
 export default function({ required, egoURL = process.env.EGO_API }) {
   if (!egoURL) {
     throw new Error(
-      'must provide ego url with either the `EGO_API` env variable or egoURL argument',
+      "must provide ego url with either the `EGO_API` env variable or egoURL argument"
     );
   }
 
   return async (req, res, next) => {
-    const { authorization } = req.headers;
-    const token = authorization ? authorization.split(' ')[1] : req.query.key;
+    const { authorization: authorizationHeader } = req.headers;
+    const { authorization: authorizationBody } = req.body;
+    const authorization = authorizationHeader || authorizationBody;
+
+    const token = authorization ? authorization.split(" ")[1] : req.query.key;
     const valid = token && (await verifyJWT({ token, egoURL }));
 
     if (!valid && required) {
-      res.status(401).send('unauthorized');
+      res.status(401).send("unauthorized");
     } else {
       req.jwt = { ...jwt.decode(token), valid };
       next();
