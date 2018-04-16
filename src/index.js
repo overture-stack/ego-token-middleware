702f615b85e0473aeb0f844aa7ca945859c6a46e
Minh <hlminh2000@gmail.com>
2018-04-16 11:35:57 -0400

    prettier eslint

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import jwt from "jsonwebtoken";
-import verifyJWT from "./verifyJWT";
+import jwt from 'jsonwebtoken';
+import verifyJWT from './verifyJWT';
 
 export default function({ required, egoURL = process.env.EGO_API }) {
   if (!egoURL) {
     throw new Error(
-      "must provide ego url with either the `EGO_API` env variable or egoURL argument"
+      'must provide ego url with either the `EGO_API` env variable or egoURL argument',
     );
   }
 
@@ -13,20 +13,20 @@ export default function({ required, egoURL = process.env.EGO_API }) {
     const { authorization: authorizationBody } = req.body;
     const authorization = authorizationHeader || authorizationBody;
 
-    const token = authorization ? authorization.split(" ")[1] : req.query.key;
+    const token = authorization ? authorization.split(' ')[1] : req.query.key;
 
     const onValidated = () => {
       req.jwt = { ...jwt.decode(token), valid };
       next();
     };
 
-    verifyJWT({ token: token || "", egoURL })
+    verifyJWT({ token: token || '', egoURL })
       .then(result => {
         onValidated();
       })
       .catch(err => {
         if (required) {
-          res.status(401).send("unauthorized");
+          res.status(401).send('unauthorized');
         } else {
           onValidated();
         }
--- src/verifyJWT.js
+++ src/verifyJWT.js
@@ -1,18 +1,18 @@
-import memoize from "memoizee";
-import axios from "axios";
-import ms from "ms";
-import jwt from "jsonwebtoken";
-import urlJoin from "url-join";
+import memoize from 'memoizee';
+import axios from 'axios';
+import ms from 'ms';
+import jwt from 'jsonwebtoken';
+import urlJoin from 'url-join';
 
 const getKey = memoize(
   async egoURL => {
-    const response = await axios.get(urlJoin(egoURL, "oauth/token/public_key"));
+    const response = await axios.get(urlJoin(egoURL, 'oauth/token/public_key'));
     return response.data;
   },
   {
-    maxAge: ms("3h"),
-    preFetch: true
-  }
+    maxAge: ms('3h'),
+    preFetch: true,
+  },
 );
 
 export default async function({ token, egoURL }) {
